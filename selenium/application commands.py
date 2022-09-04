6b1cf1a0838199bfaf90708349c9dcd60695a396
Anusha <anusha.greenfingers22@gmail.com>
2022-09-04 14:25:05 -0400

    relative path

--- selenium/application commands.py
+++ selenium/application commands.py
@@ -1,3 +1,5 @@
+import time
+
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
@@ -7,3 +9,11 @@ driver=webdriver.Chrome(service=serv_obj)
 
 driver.get("http://automationpractice.com/index.php")
 driver.maximize_window()
+
+print(driver.current_url)
+print(driver.title)
+print(driver.page_source)
+
+time.sleep(2)
+
+driver.close()
